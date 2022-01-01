e54a6d4c0e9b4e9aad53336b08726e2a3a042578
Teja1099 <50539940+Teja1099@users.noreply.github.com>
2022-01-01 16:45:57 +0530

    Update AmazonConfig.java

--- src/main/java/com/teja/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/teja/awsimageupload/config/AmazonConfig.java
@@ -15,8 +15,8 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3(){
         AWSCredentials awsCredentials =  new BasicAWSCredentials(
-                "AKIAV3UOLQCEMRIZK4FI",
-                "t0Mpebt9MxGPSNONMvrSP7KHwC9jSPxcIcYIS48a"
+                "access key",
+                "secret key"
         );
 
         return AmazonS3Client.builder()
